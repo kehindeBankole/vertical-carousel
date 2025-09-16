34bfef7864ba83c32de1b470d0fa8cf72b89d699
kehinde <bankolek1@gmail.com>
2025-09-16 13:22:58 +0100

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Kehinde Bankole on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
